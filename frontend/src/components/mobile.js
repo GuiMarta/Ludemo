ca9008bd52f94c7b554357c5de0defa21ba0e816
unknown <guilhermedsmarta@gmail.com>
2024-08-16 01:20:29 -0300

    mobile warningtry05

--- frontend/src/components/mobile.js
+++ frontend/src/components/mobile.js
@@ -4,14 +4,14 @@ import logo from '../pages/Home/imgs/LogoLudemo.png';
 
 const Mobile = () => {
     return (
-        <div className="mobile d-flex align-items-center justify-content-center" style={{ height: "100vh", color: "#374354", textAlign: "center", padding: "20px" }}>
+        <div className="mobile d-block align-items-center justify-content-center" style={{ height: "", color: "#374354", textAlign: "center", padding: "20px" }}>
             <h1 style={{ fontSize: "24px", marginBottom: "20px" }}>
                 A Ludemo não foi desenvolvida para este dispositivo móvel :(
             </h1>
             <img
                 className='logo-mobile'
                 style={{
-                    maxWidth: "20vh",
+                    maxWidth: "20vw",
                     filter: "drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.3))"
                 }}
                 src={logo}
