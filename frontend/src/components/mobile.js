7180f98900dedb3762199703edcd60ecb6b3313a
unknown <guilhermedsmarta@gmail.com>
2024-08-15 23:14:51 -0300

    mobile warning try03

--- frontend/src/components/mobile.js
+++ frontend/src/components/mobile.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../pages/Home/imgs/LogoLudemo.png';
 
 const Mobile = () => {
     return (
         <div className="mobile d-flex align-items-center justify-content-center" style={{ height: "100vh", color: "#374354" }}>
             <h1 style={{ fontSize: "24px" }}>A Ludemo não foi desenvolvida para este dispositivo móvel :( </h1>
+            <img src={logo} ></img>
         </div>
     )
 }
