5bc4387ee05546dff0f8ba576ce86b21c80b67ce
Larigew <larissatgew@gmail.com>
2024-09-27 17:00:25 -0300

    tela loading

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,88 +1,88 @@
-import React from 'react';
-import { useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Dashboard.css';
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import jogomemoria from '../Home/imgs/side-view-kids-playing-memory-game.jpg'
+import jogomemoria from '../Home/imgs/side-view-kids-playing-memory-game.jpg';
 import jogo2 from '../Home/imgs/medium-shot-kids-playing-memory-game.jpg';
 import jogo3 from '../Home/imgs/siblings-playing-with-brain-teaser-toys.jpg';
-import jogo4 from '../Home/imgs/ana-klipper-r8R_uJjA_mU-unsplash.jpg'
-import jogo5 from '../Home/imgs/robo-wunderkind--_QpJv4J_AA-unsplash.jpg'
-import jogo6 from '../Home/imgs/robo-wunderkind-3EuPcI31MQU-unsplash.jpg'
+import jogo4 from '../Home/imgs/ana-klipper-r8R_uJjA_mU-unsplash.jpg';
+import jogo5 from '../Home/imgs/robo-wunderkind--_QpJv4J_AA-unsplash.jpg';
+import jogo6 from '../Home/imgs/robo-wunderkind-3EuPcI31MQU-unsplash.jpg';
 import HeaderDashboard from '../../pages/Dashboard/header';
 import Footer from '../../components/footer';
 
-
 function Dashboard() {
-    const navigate = useNavigate();  // Inicializa o useNavigate
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);  // Estado de carregamento
 
     useEffect(() => {
         const validarToken = async () => {
             const token = localStorage.getItem("token");
             if (!token) {
                 console.error("TOKEN NÃO ENCONTRADO");
-                navigate('/Login');  // Redireciona para a página de login se o token não tiver no localStorage
+                navigate('/Login');  // Redireciona para a página de login se o token não existir
                 return;
             }
 
             try {
                 const res = await axios.get('https://ludemo-api.vercel.app/verifyToken', {
                     headers: {
-                        'authorization': token //manda o token do localStorage para o backend
+                        'authorization': token
                     }
                 });
 
                 if (res.data.valid) {
                     console.log('Token válido');
-                    //se der tudo certo
+                    setIsLoading(false);  // Habilita o render quando o token for válido
                 } else {
                     console.error('Token inválido');
-                    navigate('/login');  // Redireciona para a página de login se voltar token inválido
+                    navigate('/login');
                 }
             } catch (error) {
                 console.error('Erro ao verificar o token:', error);
-                navigate('/login');  // Redireciona para a página de login em caso de erro
+                navigate('/login');
             }
         };
 
-        validarToken();  // Chama a função de validação do token ao carregar a página
-    }, [navigate]);  // O hook será executado uma vez ao montar o componente, devido à dependência `navigate`
-    
-    
+        validarToken();
+    }, [navigate]);
+
+    if (isLoading) {
+        // Exibe algo enquanto verifica o token
+        return <div>Carregando...</div>;
+    }
+
     return (
-    <div>
+        <div>
+            <HeaderDashboard />
 
-        <HeaderDashboard />
+            <div className="teste">
+                <p>JOGANDO COM AS EMOÇÕES</p>
+            </div>
+            
+            <div className="grid mb-5">
+                <Link to="/jogo" className="card-link">
+                    <div className="card1"><img src={jogo6} /></div>
+                </Link>
+                <Link to="/jogo" className="card-link">
+                    <div className="card1"><img src={jogo6} /></div>
+                </Link>
+                <Link to="/jogo" className="card-link">
+                    <div className="card1"><img src={jogo6} /></div>
+                </Link>
+                <Link to="/jogo" className="card-link">
+                    <div className="card1"><img src={jogo6} /></div>
+                </Link>
+                <Link to="/jogo" className="card-link">
+                    <div className="card1"><img src={jogo6} /></div>
+                </Link>
+                <Link to="/jogo" className="card-link">
+                    <div className="card1"><img src={jogo6} /></div>
+                </Link>
+            </div>
 
-        <div className="teste">
-            <p>JOGANDO COM AS EMOÇÕES</p>
+            <Footer />
         </div>
-        
-        <div className="grid mb-5">
-            <Link to="/jogo" className="card-link">
-                <div className="card1"><img src={jogo6} /></div>
-            </Link>
-            <Link to="/jogo" className="card-link">
-                <div className="card1"><img src={jogo6} /></div>
-            </Link>
-            <Link to="/jogo" className="card-link">
-                <div className="card1"><img src={jogo6} /></div>
-            </Link>
-            <Link to="/jogo" className="card-link">
-                <div className="card1"><img src={jogo6} /></div>
-            </Link>
-            <Link to="/jogo" className="card-link">
-                <div className="card1"><img src={jogo6} /></div>
-            </Link>
-            <Link to="/jogo" className="card-link">
-                <div className="card1"><img src={jogo6} /></div>
-            </Link>
-        </div  >
-           <Footer /> 
-        </div>
-        
-
     );
 }
 
