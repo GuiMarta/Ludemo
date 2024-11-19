921ddbb8ddf18b34532a2b569699b6f62d79ea37
GuilhermeMarta <85751146+GuiMarta@users.noreply.github.com>
2024-11-19 01:11:52 -0300

    Ajuste Layout

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -41,16 +41,16 @@ function Login() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        
+
         // Valida os valores do formulário
         const validationErrors = validaLogin(values);
         setErrors(validationErrors);
-        
+
         //Se houver erros de validação, exibe no console
-        if (Object.keys(validationErrors).length > 0){
+        if (Object.keys(validationErrors).length > 0) {
             console.log('Erros de Validação:', validationErrors);
         }
-        
+
         // Verifica se todos os erros estão vazios
         const hasErrors = Object.values(validationErrors).some(error => error !== '');
 
@@ -62,19 +62,19 @@ function Login() {
                     crp: values.crp,
                     senha: values.password,
                 };
-                
+
                 console.log('Enviando dados para o servidor:', data);
                 const response = await axios.post('https://ludemo-api.vercel.app/login', data);
                 // const response = await axios.post('http://localhost:5000/login', data);
-                
+
                 console.log('Resposta do servidor:', response.data);
                 if (response.status === 204) {
-                    setMensagemErro('Usuário não encontrado, verifique as credenciais.');   
+                    setMensagemErro('Usuário não encontrado, verifique as credenciais.');
                 }
                 if (response.data.Login === true) {
                     setMensagemSucesso('Login realizado com sucesso!');
                     setMensagemErro('');
-                    
+
                     //guardando algumas coisinhas no localStorage:
                     localStorage.setItem('token', response.data.token);
                     localStorage.setItem('nome', response.data.result[0].nome);
@@ -82,12 +82,12 @@ function Login() {
                     localStorage.setItem('email', response.data.result[0].email);
                     localStorage.setItem('idProfissional', response.data.result[0].idprofissional);
 
-                    setTimeout(() => {navigate('/dashboard'); }, 1500);
+                    setTimeout(() => { navigate('/dashboard'); }, 1500);
 
                 }
-        
-                
-            } 
+
+
+            }
             catch (error) {
                 alert('API não disponível no momento, tentando reconectar...');
                 console.error('Erro ao autenticar usuário:', error);
@@ -113,43 +113,48 @@ function Login() {
                     <Link to='/' className='dois'>Página Incial</Link>
                 </div>
             </header>
-        
-        <div className='main-body'>
-            <div className='d-flex justify-content-center align-items-center vh-100'>
-                <div className='bg-white p-3 rounded h-70 '>
-                  <h1>Login</h1>
-                    <form action='' onSubmit={handleSubmit}>
-                        <div className='mb-3'>
-                            <label htmlFor='crp'><strong>CRP</strong></label>
-                            <input type='text' name='crp' onChange={handleInput} placeholder='CRP do Profissional' className='form-control rounded-0' />
-                            <span>{errors.crp && <span className='text-danger' > {errors.crp} </span> }</span>
-                        </div>
-                        <div className='mb-3'>
-                            <label htmlFor='password'><strong>Senha</strong></label>
-                            <input type='password' name='password' onChange={handleInput} placeholder='Senha' className='form-control rounded-0' />
-                            <span>{errors.password && <span className='text-danger' > {errors.password} </span> }</span>
-                        </div>
 
-                        {loading ? (
-                            <div className="text-center">
-                                <Spinner animation="border" role="status">
-                                    <span className="visually-hidden">Carregando...</span>
-                                </Spinner>
+            <div className='main-body'>
+                <div className='d-flex justify-content-center align-items-center vh-100'>
+                    <div className='bg-white p-3 rounded h-70 '>
+                        <h1>Login</h1>
+                        <form className='align' action='' onSubmit={handleSubmit}>
+                            <div className='mb-3'>
+                                <label htmlFor='crp'><strong>CRP</strong></label>
+                                <input type='text' name='crp' onChange={handleInput} placeholder='CRP do Profissional' className='form-control rounded-0' />
+                                <span>{errors.crp && <span className='text-danger' > {errors.crp} </span>}</span>
                             </div>
-                        ) : (
-                            <button type='submit' className='btn btn-success w-100'><strong>Login</strong></button>
-                        )}
+                            <div className='mb-3'>
+                                <label htmlFor='password'><strong>Senha</strong></label>
+                                <input type='password' name='password' onChange={handleInput} placeholder='Senha' className='form-control rounded-0' />
+                                <span>{errors.password && <span className='text-danger' > {errors.password} </span>}</span>
+                            </div>
+
+                            {loading ? (
+                                <div className="text-center">
+                                    <Spinner animation="border" role="status">
+                                        <span className="visually-hidden">Carregando...</span>
+                                    </Spinner>
+                                </div>
+                            ) : (
+                                <div className="d-flex justify-content-center  ">
+                                    <button type='submit' className='btn btn-success w-100'><strong>Login</strong></button>
+                                </div>
+                            )}
 
 
-                        <p className='text-danger mt-3' >{mensagemErro}</p>
-                        <p className='pt-1 small' >Ludemo.com a melhor plataforma de auxilio profissional.</p>
-                        <p className='text-success' >{mensagemSucesso}</p>
-                        <Link to="/Cadastrar" className='btn btn-default border w-100 bg-light text-decoration-none'>Cadastrar</Link>
-                    </form>
+                            <p className='text-danger mt-3' >{mensagemErro}</p>
+                            <p className='pt-1 small' >Ludemo.com a melhor plataforma de auxilio profissional.</p>
+                            <p className='text-success' >{mensagemSucesso}</p>
+                            <div className="d-flex justify-content-center  ">
+                            <Link to="/Cadastrar" className='btn btn-default border w-100 bg-light text-decoration-none'>Cadastrar</Link>
+                            </div>
+                                
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </div>
 
     );
